027dfea01d62a81754c658b0280f0dfea3a0a054
fla <frederik_oelund@outlook.com>
2025-05-09 12:37:10 +0200

    reverting bac kto old

--- s3.py
+++ s3.py
@@ -37,9 +37,11 @@ initial_zoom = 5  # Initial zoom level
 # Assuming df_filtered is a DataFrame containing filtered data
 user_input = st.multiselect("Select trajectories from filtered data", df_filtered["trajectory_id"])
 
+first_traj = user_input.pop(0)
+ini_data = pd.read_csv('{}.csv'.format(first_traj))
 # Initial empty figure to hold all the trajectories
-fig = px.line_map(pd.DataFrame(columns=["lat", "lon","Name", "TS", "SOG", "Destination", "IMO", "MMSI"]), lat="lat", lon="lat", hover_name="MMSI", hover_data=["Name", "TS", "SOG", "Destination", "IMO"],
-                  color_discrete_sequence=["red"])
+fig = px.line_map(ini_data, lat='lat', lon='lon', hover_name="MMSI", hover_data=["Name","TS","SOG", "Destination", "IMO"],
+                        color_discrete_sequence=["red"], zoom=5, height=600)
 
 # Check if any trajectories are selected
 if user_input:
@@ -53,25 +55,12 @@ if user_input:
             lat='lat', 
             lon='lon', 
             hover_name=None,  # Optional to disable hover info
-            color_discrete_sequence=["red"],  # Different color for the trajectory line
+            color_discrete_sequence=["blue"],  # Different color for the trajectory line
         )
         
         # Add the traces of each trajectory figure to the main figure
         for trace in trajectory_fig.data:
             fig.add_trace(trace)
-    
-    # Finalize the layout of the map
-    fig.update_layout(
-    mapbox=dict(
-        style="carto-darkmatter",
-        center=dict(lat=initial_lat, lon=initial_lon),
-        zoom=initial_zoom
-    )
-)
-
-
-
-
     
     # Display the figure in Streamlit
     st.plotly_chart(fig)
